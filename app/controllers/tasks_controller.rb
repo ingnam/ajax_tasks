fcce51a934bfdc7b8393f623d0b585d1f370892e
Madan Ingnam <madingnm@gmail.com>
2015-01-27 23:04:48 +0545

    respond to do |format| added

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -9,11 +9,30 @@ class TasksController < ApplicationController
 	end
 
 	def create
+		@task = Task.new(task_params)
+		@task.save
+		# respond to
+		respond to do |format|
+			format.html { redirect_to root_url }
+			# no block passed to format.js because i want to render out template for this .i.e. create.js.erb 
+			format.js
+		end 
 		
+	end
+
+	def update
+		@task = Task.find(params[:id])
+		@task.update_attributes(task_params)
+		redirect_to root_path
+	end
+
+	def destroy
+		Task.find(params[:id]).destroy
+		redirect_to root_path
 	end
 
 	private
 	def task_params
-		
+		params.require(:task).permit(:name, :complete)
 	end
 end
